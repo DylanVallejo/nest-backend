794fa3342565b59405160e2aa26ef3d6e1c73b68
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-24 13:13:55 -0500

    - addede dependecys for mongoose and .env variables - added a new template resource for authentification - declared the configmodule and mongoosemodule in the app module for the corresponding use also taking the .env url connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
+import { env } from 'process';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),// conexion hacia mongodb
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env)
+  }
+  
+}
