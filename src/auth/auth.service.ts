78ed8c2e176af9ecfb4ce8895b5b09eae1583db9
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-26 11:22:43 -0500

    - added dependency bcrytjs - modified the logic for asaving user using bcrypt to has the passwords - returning the saved user whitout the field password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
+
+
 
 @Injectable()
 export class AuthService {
@@ -19,8 +24,21 @@ export class AuthService {
     // 3 generar el jwt
     //manejar las excepciones
     try{
-      const newUser = new this.userModel( createUserDto );
-      return await newUser.save();  
+      const  {password, ...userData}  = createUserDto;
+      
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      })
+      
+      // const newUser = new this.userModel( createUserDto );
+      await newUser.save();
+      
+      // retornando el usuario sin la contraseña
+      const {password: _, ...user} = newUser.toJSON();
+      
+      return user;
+      
     }catch(error){
       if( error.code === 11000 ){
         throw new BadRequestException(`${ createUserDto.email } already exist!`)
